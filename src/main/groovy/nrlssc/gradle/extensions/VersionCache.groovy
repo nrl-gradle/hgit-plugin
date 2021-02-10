748f37b6fbefb2bc22b65ee8335cd5de877343c6
Sean Craft <sean.craft@nrlssc.navy.mil>
2021-02-10 17:23:12 -0600

    testing logger

--- src/main/groovy/nrlssc/gradle/extensions/VersionCache.groovy
+++ src/main/groovy/nrlssc/gradle/extensions/VersionCache.groovy
@@ -1,8 +1,12 @@
 package nrlssc.gradle.extensions
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 enum VersionCache {
     Instance;
 
+    private static Logger logger =  LoggerFactory.getLogger(VersionCache.class)
 
     Map<String, String> versionMap = new HashMap<>();
     VersionCache(){
@@ -14,7 +18,9 @@ enum VersionCache {
     }
 
     String get(String vcsRoot){
-        return versionMap.get(vcsRoot)
+        String version = versionMap.get(vcsRoot)
+        logger.lifecycle("Retrived $version from VersionCache using $vcsRoot")
+        return version
     }
 
     boolean contains(String vcsRoot)
